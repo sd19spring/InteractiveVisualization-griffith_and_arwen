daf68b061b13e2f6081397221e2970916bf5a0aa
Griffith-Stites <gjstites@gmail.com>
2019-03-13 16:53:46 -0400

    Add door location finding method

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ class World():
         width: The width of the world in cells
         height: The height of the world in cells
         cell_size: The dimensions of the cell in pixels"""
-        pygame.init() # initialize pygame
+        pygame.init() # initialize the pygame module
         screen_size = (height * cell_size, width * cell_size)
         self.screen = pygame.display.set_mode(screen_size)
         # set the dimensions of the world
@@ -19,8 +19,35 @@ class World():
 
     def _draw_background(self):
         """Sets the background color"""
-        COLOR = (252, 216, 169)
+        COLOR = (252, 216, 169) # the beige from the legend of zelda games
         self.screen.fill(COLOR)
+
+    def _door_location(self, door_position = 'top-center', door_width = 1):
+        """Determine the opening location, the places not to place wall
+        tiles as a border
+        door_position: The position of the door (top, bottom, left, right; dash;
+        center, left, right). Also accepts random numbers 0-
+        door_width: The width of the door in cells
+
+        returns: Tuple of the position of the opening
+
+        The following doctest was added to test if the method returns the correct
+        position for the default state.
+        >>> world = World()
+        >>> world._door_location()
+        (10.0, 0, 1)
+
+        The following doctest was added to check if could get the door width and
+        a position for a non-default state.
+        >>> world = World()
+        >>> world._door_location('top-left', 2)
+        (1, 0, 2)"""
+        pos = {
+            'top-left': (1, 0, door_width),
+            'top-center':(self.width/2, 0, door_width),
+            'top-right': (self.width-1, 0, door_width)
+        }
+        return pos.get(door_position)
 
     def _redraw(self):
         """Updates the world view"""
@@ -37,5 +64,8 @@ class World():
                     running = False
 
 if __name__ == "__main__":
-    world = World() # initalize the world
-    world.main_loop() # update graphics and check for events
+    # world = World() # initalize the world
+    # world.main_loop() # update graphics and check for events
+
+    import doctest
+    doctest.run_docstring_examples(World._door_location, globals())
