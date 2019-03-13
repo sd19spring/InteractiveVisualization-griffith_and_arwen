76c1a5c96ef5d79b992bbecb4af8e9e407e86ef5
Griffith-Stites <gjstites@gmail.com>
2019-03-13 18:02:24 -0400

    Add documentation and comments to explain pulled in code

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ class World():
     """Grid world that contains the player, wall, sludge, and the npcs.
     The information to make this class was heavily based on information
     learned from the ai-toolbox written by Dennis Chen."""
+
     def __init__(self, width=20, height = 20, cell_size=50):
         """Initialize the world.
         width: The width of the world in cells
@@ -19,20 +20,32 @@ class World():
         self.height = height
         self.cell_size = cell_size
         self._init_cells() # creates the cells
+
     def _draw_background(self):
         """Sets the background color"""
         COLOR = (252, 216, 169) # the beige from the legend of zelda games
         self.screen.fill(COLOR)
-    def _init_cells(self): # creates the cells
+
+    def _init_cells(self):
+        """Creates all of the cells, getting positions for each cell"""
         self.cells = {}
         cell_size = (self.cell_size, self.cell_size)
-        for i in range(self.height):
-            for j in range(self.width):
-                cell_coord = (i * self.cell_size, j * self.cell_size)
-                self.cells[(i, j)] = Cell(self.screen, cell_coord, cell_size)
+        for i in range(self.height): # go through all rows
+            for j in range(self.width): # go through all columns in the row
+                cell_coord = (i * self.cell_size, j * self.cell_size) # get the coordinate of that cell
+                self.cells[(i, j)] = Cell(self.screen, cell_coord, cell_size) # create the cell and add it to the list
 
     def _add_coords(self, a, b):
-        """Return a third coord that is equivalent to (a[0]+b[0], a[1]+b[1])."""
+        """Rewrites the current location to the place it needs to go.
+
+        a: current position of the item
+        b: the position the item is going to
+
+        zip rewrites each element in a and b as pairs based on index:
+        ((a[0], b[0]), (a[1], b[1]))
+        Then, we take the sum of each of those pairs, and rewrite it as a tuple:
+        (a[0]+b[0], a[1]+b[1])
+        """
         return tuple(map(sum, zip(a, b)))
 
     def _door_location(self, door_position = random.randint(1, 4)):
@@ -46,13 +59,13 @@ class World():
         The following doctest was added to test all four positions
         >>> world = World()
         >>> world._door_location(1)
-        (10, 0, 1)
+        (10, 0)
         >>> world._door_location(2)
-        (10, 20, 1)
+        (10, 19)
         >>> world._door_location(3)
-        (0, 10, 1)
+        (0, 10)
         >>> world._door_location(4)
-        (20, 10, 1)"""
+        (19, 10)"""
         pos = {
             1:(int(self.width/2), 0), # center top door
             2:(int(self.width/2), self.height-1), # center bottom door
@@ -127,6 +140,7 @@ class Cell():
         self.dimensions = dimensions
 
     def draw(self):
+        """Draws a cell onto the background"""
         self.draw_screen.blit(self.coordinates)
 
 
